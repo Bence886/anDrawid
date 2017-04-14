ba056257165745cfcf9c3d88a38ea7a4a5d7aff4
Tóth Bence <tbencet96@gmail.com>
2017-04-14 16:11:54 +0200

    Color picker transitioned to fragment

--- app/src/main/java/acceptable_risk/nik/uniobuda/hu/andrawid/ColorPickerActivity.java
+++ app/src/main/java/acceptable_risk/nik/uniobuda/hu/andrawid/ColorPickerActivity.java
@@ -1,12 +1,16 @@
 package acceptable_risk.nik.uniobuda.hu.andrawid;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
@@ -14,17 +18,25 @@ import android.widget.SeekBar;
  * Created by tbenc on 2017. 04. 11..
  */
 
-public class ColorPickerActivity extends AppCompatActivity {
-    Button ok_btn, cancel_btn;
-    ImageView color;
-    SeekBar S_A, S_R, S_G, S_B;
+public class ColorPickerActivity extends AppCompatActivity{
 
-    int a=255, r=1, g=1, b=1;
+    Button cancel_btn, ok_btn;
+
+    ColorPickerFragment cpf;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.color_picker_activity_layout);
+
+        Bundle args = new Bundle();
+
+        cpf = new ColorPickerFragment().newInstance(args);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_control, cpf);
+        transaction.commit();
 
         cancel_btn = (Button) findViewById(R.id.cancel_btn);
         cancel_btn.setOnClickListener(new View.OnClickListener() {
@@ -33,56 +45,20 @@ public class ColorPickerActivity extends AppCompatActivity {
                 finish();
             }
         });
-        color = (ImageView) findViewById(R.id.color);
-
-        S_A = (SeekBar) findViewById(R.id.a);
-        S_R = (SeekBar) findViewById(R.id.r);
-        S_G = (SeekBar) findViewById(R.id.g);
-        S_B = (SeekBar) findViewById(R.id.b);
-
-        S_A.setOnSeekBarChangeListener(bar);
-        S_R.setOnSeekBarChangeListener(bar);
-        S_G.setOnSeekBarChangeListener(bar);
-        S_B.setOnSeekBarChangeListener(bar);
 
         ok_btn = (Button) findViewById(R.id.ok_btn);
         ok_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent output = new Intent();
-                output.putExtra("A", a);
-                output.putExtra("R", r);
-                output.putExtra("G", g);
-                output.putExtra("B", b);
+                output.putExtra("A", cpf.getArguments().getInt("A"));
+                output.putExtra("R", cpf.getArguments().getInt("R"));
+                output.putExtra("G", cpf.getArguments().getInt("G"));
+                output.putExtra("B", cpf.getArguments().getInt("B"));
                 setResult(RESULT_OK, output);
                 finish();
             }
         });
     }
-
-    SeekBar.OnSeekBarChangeListener bar = new SeekBar.OnSeekBarChangeListener() {
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            if (seekBar==S_A)
-                a=progress;
-            if (seekBar==S_R)
-                r=progress;
-            if (seekBar==S_G)
-                g=progress;
-            if (seekBar==S_B)
-                b=progress;
-
-            color.getDrawable().setColorFilter(new Color(a, r, g, b).colorint, PorterDuff.Mode.ADD);
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-
-        }
-    };
 }
---
+++ app/src/main/java/acceptable_risk/nik/uniobuda/hu/andrawid/ColorPickerFragment.java
@@ -0,0 +1,94 @@
+package acceptable_risk.nik.uniobuda.hu.andrawid;
+
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import static android.app.Activity.RESULT_OK;
+
+/**
+ * Created by tbenc on 2017. 04. 14..
+ */
+
+public class ColorPickerFragment extends Fragment {
+    View rootView;
+
+    ImageView color;
+    SeekBar S_A, S_R, S_G, S_B;
+
+    int a=255, r=1, g=1, b=1;
+
+
+    public static ColorPickerFragment newInstance(Bundle arg) {
+        
+        Bundle args =arg;
+        
+        ColorPickerFragment fragment = new ColorPickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.color_picker_fragment_layout, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        color = (ImageView) rootView.findViewById(R.id.color);
+
+        S_A = (SeekBar) rootView.findViewById(R.id.a);
+        S_R = (SeekBar) rootView.findViewById(R.id.r);
+        S_G = (SeekBar) rootView.findViewById(R.id.g);
+        S_B = (SeekBar) rootView.findViewById(R.id.b);
+
+        SeekBar.OnSeekBarChangeListener bar = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (seekBar == S_A)
+                    a = progress;
+                if (seekBar == S_R)
+                    r = progress;
+                if (seekBar == S_G)
+                    g = progress;
+                if (seekBar == S_B)
+                    b = progress;
+
+                getArguments().clear();
+                getArguments().putInt("A", a);
+                getArguments().putInt("R", r);
+                getArguments().putInt("G", g);
+                getArguments().putInt("B", b);
+
+                color.getDrawable().setColorFilter(new Color(a, r, g, b).colorint, PorterDuff.Mode.ADD);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+
+        S_A.setOnSeekBarChangeListener(bar);
+        S_R.setOnSeekBarChangeListener(bar);
+        S_G.setOnSeekBarChangeListener(bar);
+        S_B.setOnSeekBarChangeListener(bar);
+    }
+}
