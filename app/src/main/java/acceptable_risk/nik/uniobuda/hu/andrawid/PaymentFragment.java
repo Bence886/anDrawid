19d70ac90cb11eb16a67f6a6d3c0b7210340f282
Toth Bence <tbencet96@gmail.com>
2017-04-15 21:16:56 +0200

    gépet váltok

--- app/src/main/java/acceptable_risk/nik/uniobuda/hu/andrawid/PaymentFragment.java
+++ app/src/main/java/acceptable_risk/nik/uniobuda/hu/andrawid/PaymentFragment.java
@@ -1,11 +1,18 @@
 package acceptable_risk.nik.uniobuda.hu.andrawid;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.vending.billing.IMyAidlInterface;
 
 /**
  * Created by tbenc on 2017. 04. 14..
@@ -14,6 +21,7 @@ import android.view.ViewGroup;
 public class PaymentFragment extends Fragment {
 
     View rootView;
+    IMyAidlInterface mService;
 
     public static PaymentFragment newInstance() {
 
@@ -22,6 +30,14 @@ public class PaymentFragment extends Fragment {
         PaymentFragment fragment = new PaymentFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Intent serviceIntent =
+                new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        getActivity().bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
     }
 
     @Nullable
@@ -40,12 +56,31 @@ public class PaymentFragment extends Fragment {
         SuccessfulPayment();
     }
 
-
-
     //TODO: call this                      --!!--
     private void SuccessfulPayment()
     {
         getArguments().clear();
         getArguments().putBoolean("payed", true);
+    }
+
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name,
+                                       IBinder service) {
+            mService = IMyAidlInterface.Stub.asInterface(service);
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            getActivity().unbindService(mServiceConn);
+        }
     }
 }
