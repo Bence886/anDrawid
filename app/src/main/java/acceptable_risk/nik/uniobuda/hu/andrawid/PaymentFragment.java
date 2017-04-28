b1f4cfa8169d40bef592b6a2b2068d1f5bfb30bc
Tóth Bence <tbencet96@gmail.com>
2017-04-29 00:52:51 +0200

    Inapp Billing setup

--- app/src/main/java/acceptable_risk/nik/uniobuda/hu/andrawid/PaymentFragment.java
+++ app/src/main/java/acceptable_risk/nik/uniobuda/hu/andrawid/PaymentFragment.java
@@ -12,7 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.android.vending.billing.IMyAidlInterface;
+import com.android.vending.billing.IInAppBillingService;
+
+import acceptable_risk.nik.uniobuda.hu.andrawid.util.IabHelper;
+import acceptable_risk.nik.uniobuda.hu.andrawid.util.IabResult;
+import acceptable_risk.nik.uniobuda.hu.andrawid.util.Purchase;
 
 /**
  * Created by tbenc on 2017. 04. 14..
@@ -20,8 +24,14 @@ import com.android.vending.billing.IMyAidlInterface;
 
 public class PaymentFragment extends Fragment {
 
+    IInAppBillingService mService;
+
+    IabHelper mHelper;
+
     View rootView;
-    IMyAidlInterface mService;
+
+    //ToDo: Hash It m8!!
+    private String myPublicKey = "AIzaSyCcFUmW0j_wxmA9Ww7GMl-VV3FwxEut1oE";
 
     public static PaymentFragment newInstance() {
 
@@ -41,6 +51,18 @@ public class PaymentFragment extends Fragment {
         getActivity().bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
     }
 
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = IInAppBillingService.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+    };
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -52,9 +74,45 @@ public class PaymentFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        mHelper = new IabHelper(getActivity(), myPublicKey);
+
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            @Override
+            public void onIabSetupFinished(IabResult result) {
+                if (mHelper== null) return;
+                if (!result.isSuccess()) return;
+            }
+        });
+
+        //ToDo: Hash This Too M8!
+        String payload = "";
+        mHelper.launchPurchaseFlow(getActivity(), "OneTime", 100001, mPurchaseFinishedListener, payload);
+
         //TODO: getViews here
-        SuccessfulPayment();
+
+        //SuccessfulPayment();
     }
+
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        @Override
+        public void onIabPurchaseFinished(IabResult result, Purchase info) {
+            if (mHelper==null)
+                return;
+
+            if (result.isFailure()){
+                return;
+            }
+
+            //ToDo: Some more hashing.
+            String payload = "";
+            if (info.getDeveloperPayload() != payload)
+                return;
+
+            if (info.getSku().equals("OneTime")){
+                SuccessfulPayment();
+            }
+        }
+    };
 
     //TODO: call this                      --!!--
     private void SuccessfulPayment()
@@ -63,24 +121,10 @@ public class PaymentFragment extends Fragment {
         getArguments().putBoolean("payed", true);
     }
 
-    ServiceConnection mServiceConn = new ServiceConnection() {
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            mService = null;
-        }
-
-        @Override
-        public void onServiceConnected(ComponentName name,
-                                       IBinder service) {
-            mService = IMyAidlInterface.Stub.asInterface(service);
-        }
-    };
-
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (mService != null) {
+        if (mService!=null)
             getActivity().unbindService(mServiceConn);
-        }
     }
 }
